5fe33731de3322c85add9b5668f2d0143b99452f
Peder Antonisen <113884778+ShadySloth@users.noreply.github.com>
2025-03-12 14:15:09 +0100

    feat: upload to cleaner

--- frontend/Search-Enron/src/app/app.component.ts
+++ frontend/Search-Enron/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {throwError} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -8,8 +10,55 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Search-Enron';
   searchTerm: string = '';
+  status: "initial" | "uploading" | "success" | "fail" = "initial";
+  files: File[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+  }
 
   search() {
 
+  }
+
+  onChange(event: any) {
+    const files = event.target.files;
+
+    if (files.length) {
+      this.status = "initial";
+      this.files = files;
+    }
+  }
+
+  onUpload() {
+    if (this.files.length) {
+      const formData = new FormData();
+
+      [...this.files].forEach((file) => {
+        formData.append("files", file, file.name)
+      });
+
+      const upload$ = this.http.post("http://localhost:5110/api/cleaner/clean", formData);
+
+      this.status = "uploading";
+
+      upload$.subscribe({
+        next: () => {
+          this.status = "success";
+        },
+        error: (error: any) => {
+          this.status = "fail";
+          console.log(error);
+          return throwError(() => error);
+        }
+      })
+    }
+  }
+
+  onClear() {
+    this.status = "initial";
+    this.files = [];
   }
 }
