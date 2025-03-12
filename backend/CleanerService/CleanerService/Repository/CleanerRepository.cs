c8b8c84e95ee12cd0869b0c53eaeac0ea818a66b
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2025-03-12 11:02:39 +0100

    feat: Implemented circuitbreaker pattern
    implemented in Cleaner service, think that is only one where it would be good

--- backend/CleanerService/CleanerService/Repository/CleanerRepository.cs
+++ backend/CleanerService/CleanerService/Repository/CleanerRepository.cs
@@ -2,30 +2,47 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using CleanerService.Models;
+using Polly;
+using Polly.CircuitBreaker;
 
 namespace CleanerService.Repository;
 
 public class CleanerRepository : ICleanerRepository
 {
     private readonly HttpClient _httpClient;
+    private AsyncCircuitBreakerPolicy _circuitBreakerPolicy;
+    
 
     public CleanerRepository(HttpClient httpClient)
     {
         _httpClient = httpClient;
+        _circuitBreakerPolicy = Policy.Handle<Exception>()
+            .CircuitBreakerAsync(3, TimeSpan.FromSeconds(30),
+                (ex, ts) =>
+                {
+                    Console.WriteLine($"Circuit broken: sleep {ts.TotalSeconds} seconds");
+                },
+                () =>
+                {
+                    Console.WriteLine("Circuit reset");
+                });
     }
 
     public async Task SendCleanFiles(List<CleanedFileDto> files)
     {
-        JsonSerializerOptions options = new()
+        await _circuitBreakerPolicy.ExecuteAsync(async () =>
         {
-            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-        };
+            JsonSerializerOptions options = new()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            };
 
-        var json = JsonSerializer.Serialize(new { Files = files }, options);
+            var json = JsonSerializer.Serialize(new { Files = files }, options);
 
-        using StringContent jsonContent = new(json, Encoding.UTF8, "application/json");
+            using StringContent jsonContent = new(json, Encoding.UTF8, "application/json");
 
-        await _httpClient.PostAsync("/api/database/insert", jsonContent);
+            await _httpClient.PostAsync("/api/database/insert", jsonContent);
+        });
     }
 }
