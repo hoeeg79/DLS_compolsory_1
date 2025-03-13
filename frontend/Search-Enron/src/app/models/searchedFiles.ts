ee495e56b308b05c83a1acbc0be584bf2a140a28
ShadySloth <pedant01@easv365.dk>
2025-03-13 10:20:05 +0100

    feat: search and download

---
+++ frontend/Search-Enron/src/app/models/searchedFiles.ts
@@ -0,0 +1,10 @@
+﻿export interface SearchedFiles {
+  files: DownloadFile[];
+  searchQuery: string;
+}
+
+export interface DownloadFile {
+  fileName: string;
+  fileContent: string;
+  countOfOccurrences: number;
+}
